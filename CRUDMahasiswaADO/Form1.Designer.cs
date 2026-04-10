be2b67752580aac089fe4013184cd088d431b09a
RifalArdi25 <rifalardi25@gmail.com>
2026-04-10 11:33:47 +0700

    Membuat methpod untuk menampilkan data ke form saat baris dipilih

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -207,6 +207,7 @@
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(700, 150);
             this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Form1
             // 
