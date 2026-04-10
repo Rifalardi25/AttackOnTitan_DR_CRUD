ba4b7d130fd6353f36a7ab8ae4494e80e4f932f4
RifalArdi25 <rifalardi25@gmail.com>
2026-04-10 10:21:33 +0700

    Membuat Variabel Koneksi

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_koneksi = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -133,14 +133,15 @@
             this.textBox4.Size = new System.Drawing.Size(250, 50);
             this.textBox4.TabIndex = 9;
             // 
-            // button1
+            // btn_koneksi
             // 
-            this.button1.Location = new System.Drawing.Point(550, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 30);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Membuka Koneksi";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_koneksi.Location = new System.Drawing.Point(550, 30);
+            this.btn_koneksi.Name = "btn_koneksi";
+            this.btn_koneksi.Size = new System.Drawing.Size(150, 30);
+            this.btn_koneksi.TabIndex = 10;
+            this.btn_koneksi.Text = "Membuka Koneksi";
+            this.btn_koneksi.UseVisualStyleBackColor = true;
+            this.btn_koneksi.Click += new System.EventHandler(this.btn_koneksi);
             // 
             // button2
             // 
@@ -215,7 +216,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_koneksi);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -246,7 +247,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_koneksi;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -14,12 +14,21 @@ namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+        "Data Source=ADVAN\\RIFALARDI;Initial Catalog=DBAkademikADO;Integrated Security=True"
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect(object sender, EventArgs e)
         {
 
         }
